f7f7947a68dd4b793b7283efa322ff5ed49b03db
Leosbel Poll Sotomayor <leitopoll25@gmail.com>
2019-04-12 11:03:13 -0300

    prueba de pregunta y opciones e inicio de envio

--- features/step_definitions/my_steps.rb
+++ features/step_definitions/my_steps.rb
@@ -22,3 +22,17 @@
   Then(/^Que contenga la pregunta "([^"]*)"$/) do |pregunta|
     expect(find("legend.pregunta").text).to eq pregunta 
   end
+
+  Then(/^Que contenga la opcion "([^"]*)"$/) do |respuesta|
+    expect(find("label[for='r2']").text).to eq respuesta 
+  end
+
+  Given(/^Envio de pregunta "([^"]*)" y opcion "([^"]*)"$/) do |pregunta, opcion|
+    fill_in("pregunta", with: pregunta)
+    choose "r2"
+    click_button("submit")
+  end
+  
+#   Then(/^La pagina resultado contiene "([^"]*)"$/) do |arg1|
+#     expect(find("label[for='r2']").text).to eq respuesta 
+#   end
--- features/template.feature
+++ features/template.feature
@@ -16,3 +16,12 @@ Scenario: Navego a categoria
 Scenario: Mostrar pregunta
     Given Pagina de pregunta
     Then Que contenga la pregunta "Cuanto es 2+2?"
+
+Scenario: Mostrar opciones
+    Given Pagina de pregunta
+    Then Que contenga la opcion "4"
+
+# Scenario: Validar respuesta
+#     Given Pagina de pregunta
+#     And Envio de pregunta "Cuanto es 2+2?" y opcion "4"
+#     Then La pagina resultado contiene "OK"
